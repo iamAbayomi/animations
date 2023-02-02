eafee254096012045ec7d3cc6262ba1793188c09
iamAbayomi <oladinifolawiyo@gmail.com>
2023-02-02 06:56:19 +0100

    Added elements to repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import "@/styles/animate.css";
+import "@/styles/circle.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
---
+++ src/pages/circle/index.tsx
@@ -0,0 +1,11 @@
+const Index = () => {
+  return (
+    <div id="container">
+      <div className="circle" id="circle1"></div>
+      <div className="circle" id="circle2"></div>
+      <div className="circle" id="circle3"></div>
+    </div>
+  );
+};
+
+export default Index;
